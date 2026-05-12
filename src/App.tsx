4caefe528e60a51dd30e7544743925a1d7faf7ad
“Tamara <tamara.kozok@bakers.studio>
2026-05-12 16:42:22 +0300

    chore: add prettier and eslint-config-prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return <div className="min-h-screen bg-amber-100">Initial App</div>
+  return <div className="min-h-screen bg-amber-100">Initial App</div>;
 }
 
-export default App
+export default App;
